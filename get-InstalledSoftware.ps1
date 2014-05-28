99b5a07f42b4603944d123f9e4222c5e9dcb7fc5
Craig Meinschein <cmeinsch@gmail.com>
2014-05-28 15:11:40 -0700

    Updated to use SCCM installed software list.

--- get-InstalledSoftware.ps1
+++ get-InstalledSoftware.ps1
@@ -3,32 +3,33 @@
 ##       Date: 01/15/2013
 ##    Purpose: To retrieve all of the installed software on a machine.
 ## Parameters: <empty> or <computer name>
-##                     <empty> Will produce a list of software installed on the local computer. 
-##             <computer name> Will produce a list of software of the remote computer and output it to the screen.
+##   <empty> Will produce a list of software installed on the local computer.
+##   <computer name> Will produce a list of software of the remote computer and
+##     output it to screen.
 
 Param(
     ## Retrieves the inputted computer name otherwise sets the variable to null.
     [String]$Comp=$null
 )
 
-## Checks that a computer name was entered. 
-If($Comp -eq $null){
+## Checks if a computer name is entered.
+If($Comp -eq ""){
 
     ####################### Variables ##############################
     ## Gets the name of the local computer.
     $computername = gc env:computername
     ## Gets the current date and time formatted properly.
-    $date = get-date -format "_MMddyyyy-hhmmss"
-    ## Location where the files will be stored. 
-    $sourcelocation="c:\temp"
-    ## Location where the old files will be moved. 
-    $targetlocation="c:\temp\_old"
-    ## This sets where the updated list of installed software file gets stored.
+    $date = get-date -format "_MMddyyyy-HHmmss"
+    ## Location of the where the files need to be stored. 
+    $sourcelocation="\\idunn\installedsoftware"
+    ## Location of where the old files need to be moved. 
+    $targetlocation="\\idunn\installedsoftware\_old"
+    ## This sets where the updated installed software file gets stored.
     $filename= $sourcelocation+ "\" + $computername + $date + ".txt"
-    ## This finds any files with the same name in the source locaiton.
+    ## This finds any files with the same name in the source location.
     $filefound = Get-ChildItem -Path $sourcelocation $computername* | select -expand name
 
-    ## Checks if a file already exists. If it does then it is moved to the target location (where old files go to DIE). 
+    ## Checks a file already exists. If it does then it is moved to the the target location. 
     if($filefound) {
          foreach($name in $filefound){
                     ## sets the file path of the source file.
@@ -36,13 +37,13 @@ If($Comp -eq $null){
                     Move-Item $filepath $targetlocation
          }
     }
-    ## This retrieves the list of installed software on the computer, then formats the output (installed date, version
-    ## number, program name). It then sorts the list by name and outputs it to the source location with the proper
+    ## This retrieves the installed software list of the computer, then formats the output (installed date, version
+    ## number, program name). It then sorts the list by name and then outputs it to the source location with the proper
     ## naming scheme.
-    Get-WmiObject -class win32reg_addremoveprograms | select installdate,version,displayname -Unique | sort displayname | format-table -autosize >> $filename
+    Get-WmiObject -Query "SELECT * FROM SMS_InstalledSoftware" -Namespace "root\cimv2\sms" | Select-Object @{l='InstallDate';e={$_.InstallDate.substring(0,8)}},ProductVersion,ProductName | Sort-Object ProductName | Format-Table -AutoSize | Out-File -Encoding ASCII $filename
 }else{
 
     ## This retrieves the installed software list of a remote computer, then formats the output (installed date,
-    ## version number, program name). It then sorts the list by name and outputs it to the screen.  
-    Get-WmiObject -ComputerName $Comp -class win32reg_addremoveprograms | select installdate,version,displayname -Unique | sort displayname | format-table -autosize
+    ## version number, program name). It then sorts the list by name and then outputs to the screen.  
+    Get-WmiObject -ComputerName $Comp -Query "SELECT * FROM SMS_InstalledSoftware" -Namespace "root\cimv2\sms" | Select-Object @{l='InstallDate';e={$_.InstallDate.substring(0,8)}},ProductVersion,ProductName | Sort-Object ProductName | Format-Table -AutoSize
 }
